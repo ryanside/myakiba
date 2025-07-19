0b5d001666935db48422594c9c682474f09abc53
RAlcala2 <ryanalcala10@gmail.com>
2025-07-18 23:22:24 -0400

    csv form validation + filtering

--- apps/server/src/routers/sync/index.ts
+++ apps/server/src/routers/sync/index.ts
@@ -1,22 +1,47 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { Queue } from "bullmq";
 
-export const sync = new Elysia({ prefix: "/sync" }).post("/", async () => {
-  const myQueue = new Queue("myqueue");
-  
-  try {
-    console.log("Elysia: Adding job to queue");
-    await myQueue.add("myJobName", { foo: "bar" });
-    console.log("Elysia: Job successfully added to queue");
-    
+export const sync = new Elysia({ prefix: "/sync" })
+  .post("/", async ({ body }) => {
+    console.log("Elysia: figureData:", body.figureData);
+
+    const myQueue = new Queue("myqueue");
+
+    try {
+      console.log("Elysia: Adding job to queue");
+      await myQueue.add("myJobName", { foo: "bar" });
+      console.log("Elysia: Job successfully added to queue");
+
+      return {
+        message: "Job added!",
+      };
+    } catch (error) {
+      console.error("Elysia: Failed to add job to queue:", error);
+      return {
+        message: "Failed to add job",
+        error: error instanceof Error ? error.message : String(error),
+      };
+    }
+  }, {
+    body: t.Object({
+      figureData: t.Array(t.Object({
+        id: t.String(),
+        status: t.String(),
+        count: t.Number(),
+        score: t.Number(),
+        payment_date: t.String(),
+        shipping_date: t.String(),
+        collecting_date: t.String(),
+        price: t.Number(),
+        shop: t.String(),
+        shipping_method: t.String(),
+        tracking_number: t.String(),
+        note: t.String(),
+      }))
+    })
+  })
+  .get("/", () => {
     return {
-      message: "Job added!",
+      message: "Hello World",
     };
-  } catch (error) {
-    console.error("Elysia: Failed to add job to queue:", error);
-    return {
-      message: "Failed to add job",
-      error: error instanceof Error ? error.message : String(error),
-    };
-  }
-});
+  });
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,28 +1,148 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Papa from "papaparse";
-import { useForm } from "@tanstack/react-form";
+import { useForm, useStore } from "@tanstack/react-form";
 import { useQuery } from "@tanstack/react-query";
 import { app } from "../lib/treaty-client";
+import { z } from "zod";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
 
-function HomeComponent() {
-  // const form = useForm({
-  //   defaultValues: {
-  //     file: null as File | null,
-  //   },
-  //   onSubmit: async ({ value }) => {
-  //     const file = value.file;
-  //     if (!file) return;
+const csvSchema = z.array(
+  z.object({
+    id: z.string(),
+    title: z.string(),
+    root: z.string(),
+    category: z.string(),
+    release_date: z.string(),
+    price: z.string(),
+    scale: z.string(),
+    barcode: z.string(),
+    status: z.string(),
+    count: z.string(),
+    score: z.string(),
+    payment_date: z.string(),
+    shipping_date: z.string(),
+    collecting_date: z.string(),
+    price_1: z.string(),
+    shop: z.string(),
+    shipping_method: z.string(),
+    tracking_number: z.string(),
+    wishibility: z.string().optional(),
+    note: z.string(),
+  })
+);
 
-  //     const text = await file.text();
-  //     const result = Papa.parse(text, { header: true });
-  //     const data = result.data;
-  //     console.log(data);
-  //   },
-  // });
+const fileCheck = z
+  .instanceof(File)
+  .refine((file) => file.type === "text/csv", {
+    message: "Only MFC CSV file allowed",
+  })
+  .refine((file) => file.size <= 10_000_000, {
+    message: "File size must be less than 10MB",
+  });
+
+function HomeComponent() {
+  const form = useForm({
+    defaultValues: {
+      file: undefined as File | undefined,
+    },
+    onSubmit: async ({ value }) => {
+      const startTime = Date.now();
+      console.log("onSubmit: Client-side CSV validation & filtering started");
+
+      if (!value.file) return "No file selected";
+      const text = await value.file.text();
+      const parsedCSV = Papa.parse(text, {
+        header: true,
+        skipEmptyLines: true,
+        transformHeader: (header) =>
+          header.trim().toLowerCase().replace(/ /g, "_"),
+      });
+      console.log("CSV parsed");
+      const validatedCSV = csvSchema.safeParse(parsedCSV.data);
+      if (!validatedCSV.success) {
+        console.log("Invalid CSV file", validatedCSV.error);
+        return "Please select a valid MyFigureCollection CSV file";
+      }
+      console.log("CSV validated");
+
+      const filteredData = validatedCSV.data.filter((item) => {
+        return item.status === "Owned" || item.status === "Ordered";
+      });
+
+      if (filteredData.length === 0) {
+        return "No Owned or Ordered figures to sync";
+      }
+
+      console.log("Filtered data:", filteredData);
+
+      const figureIDs = filteredData.map((item) => item.id);
+      const userFigureData = filteredData.map((item) => {
+        return {
+          id: item.id,
+          status: item.status,
+          count: Number(item.count),
+          score: Number(item.score.split("/")[0]),
+          payment_date: item.payment_date,
+          shipping_date: item.shipping_date,
+          collecting_date: item.collecting_date,
+          price: Number(item.price_1),
+          shop: item.shop,
+          shipping_method: item.shipping_method,
+          tracking_number: item.tracking_number,
+          note: item.note,
+        };
+      });
+
+      console.log("Figure IDs:", figureIDs);
+      console.log("User figure data:", userFigureData);
+
+      const response = await app.sync.post({ figureData: userFigureData });
+      console.log("Response:", response);
+
+      console.log(`onSubmit: Time taken: ${Date.now() - startTime}ms`);
+    },
+    validators: {
+      onChangeAsync: async ({ value }) => {
+        const startTime = Date.now();
+        console.log(
+          "onChangeAsync: Client-side CSV validation & filtering started"
+        );
+
+        if (!value.file) return "No file selected";
+        const text = await value.file.text();
+        const parsedCSV = Papa.parse(text, {
+          header: true,
+          skipEmptyLines: true,
+          transformHeader: (header) =>
+            header.trim().toLowerCase().replace(/ /g, "_"),
+        });
+        console.log("CSV parsed");
+        const validatedCSV = csvSchema.safeParse(parsedCSV.data);
+        if (!validatedCSV.success) {
+          console.log("Invalid CSV file", validatedCSV.error);
+          return "Please select a valid MyFigureCollection CSV file";
+        }
+        console.log("CSV validated");
+
+        const filteredData = validatedCSV.data.filter((item) => {
+          return item.status === "Owned" || item.status === "Ordered";
+        });
+
+        if (filteredData.length === 0) {
+          return "No Owned or Ordered figures to sync";
+        }
+        console.log(`onChangeAsync: Time taken: ${Date.now() - startTime}ms`);
+      },
+    },
+  });
+
+  const formErrorMap = useStore(form.store, (state) => state.errorMap);
 
   const { data, refetch, isLoading, error } = useQuery({
     queryKey: ["test-api"],
@@ -51,14 +171,45 @@ function HomeComponent() {
             </div>
           )}
           {data && (
-            <div className="text-green-600 text-sm">
-              Response: {data.data}
-            </div>
+            <div className="text-green-600 text-sm">Response: {data.data}</div>
           )}
         </section>
         <section className="rounded-lg border p-4">
           <h2 className="mb-2 font-medium">Sync with your MFC</h2>
-          <form action=""></form>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              void form.handleSubmit();
+            }}
+          >
+            <form.Field
+              name="file"
+              children={(field) => {
+                return (
+                  <div className="space-y-2">
+                    <Label htmlFor="csv-file">Select CSV File</Label>
+                    <Input
+                      id="csv-file"
+                      type="file"
+                      accept=".csv,text/csv"
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        field.handleChange(file);
+                      }}
+                      className="file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+                    />
+                    {formErrorMap.onChange ? (
+                      <div>
+                        <em role="alert">{formErrorMap.onChange}</em>
+                      </div>
+                    ) : null}
+                    <Button type="submit">Submit</Button>
+                  </div>
+                );
+              }}
+            />
+          </form>
         </section>
       </div>
     </div>
