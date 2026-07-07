ff2650a90304a2ef411780c804f6ed7a975fc575
RAlcala2 <ryanalcala10@gmail.com>
2026-07-07 15:45:31 -0400

    refactor(worker): rename HTTP_PROXY to WORKER_PROXY_URL

--- packages/env/src/worker.ts
+++ packages/env/src/worker.ts
@@ -7,7 +7,7 @@ export const env = createEnv({
   server: {
     ...redisSchema,
     ...nodeEnvSchema,
-    HTTP_PROXY: z.url().optional(),
+    WORKER_PROXY_URL: z.url().optional(),
     AWS_BUCKET_REGION: z.string().min(1),
     AWS_BUCKET_NAME: z.string().min(1),
     POSTHOG_API_KEY: z.string().min(1).optional(),
