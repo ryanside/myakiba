e3072135697920bf8650b52e5fe3d835f1fbe159
RAlcala2 <ryanalcala10@gmail.com>
2026-08-10 22:02:43 -0400

    style(charts): update colors for light and dark themes

--- apps/web/src/components/homepage/integrations-1.tsx
+++ apps/web/src/components/homepage/integrations-1.tsx
@@ -62,8 +62,8 @@ function SyncIllustration() {
 
       <div className="@lg:px-24 relative flex h-10 items-center justify-between px-12">
         <div className="bg-border absolute inset-0 my-auto h-px" />
-        <div className="bg-chart-5 mask-l-from-15% mask-l-to-40% mask-r-from-75% mask-r-to-75% absolute inset-0 my-auto h-px w-1/2" />
-        <div className="bg-chart-6 mask-r-from-15% mask-r-to-40% mask-l-from-75% mask-l-to-75% absolute inset-0 my-auto ml-auto h-px w-1/2" />
+        <div className="bg-chart-6 mask-l-from-15% mask-l-to-40% mask-r-from-75% mask-r-to-75% absolute inset-0 my-auto h-px w-1/2" />
+        <div className="bg-chart-2 mask-r-from-15% mask-r-to-40% mask-l-from-75% mask-l-to-75% absolute inset-0 my-auto ml-auto h-px w-1/2" />
 
         <FigureThumb src={FIGURE_ITEMS[2]} />
 
